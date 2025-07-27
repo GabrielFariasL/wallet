322c37a01188e9e27687c7102135422d29657176
Gabriel de Farias <Gabriellongarai.gl@gmail.com>
2025-07-27 16:48:41 -0300

    init commit

---
+++ src/main/java/br/com/wallet/demo/WalletAdministrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.wallet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wallet/demo/WalletAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wallet.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
